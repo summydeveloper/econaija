41d391c1f914d3645045a7877abef4009f58c948
summydev <adegbitesumayah@gmail.com>
2024-04-03 00:14:48 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(MyApp ());
 }
 
 class MyApp extends StatelessWidget {
